73285f06922a26b1067b4f6690c88c109795d170
Warlin <warlinsano@gmail.com>
2020-10-21 18:26:20 -0400

    added models and connection to db

---
+++ Store.Web/Models/CustomerAndSuppliersByCity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Store.Web.Models
+{
+    public partial class CustomerAndSuppliersByCity
+    {
+        public string City { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string Relationship { get; set; }
+    }
+}
