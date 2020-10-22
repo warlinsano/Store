d7073eba2d50202b453511a595c947f47cd2164e
Warlin <warlinsano@gmail.com>
2020-10-22 17:00:02 -0400

    add seedb

---
+++ Store.Web/Data/SeedDb.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Store.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Web.Data
+{
+    public class SeedDb
+    {
+        private static  DataContext _dataContext;
+
+        public static async Task Initialize(IServiceProvider serviceProvider)
+        {
+            //var context = serviceProvider.GetRequiredService<DataContext>();
+            //context.Database.Migrate();
+
+            using (var context = new DataContext(serviceProvider
+                .GetRequiredService<DbContextOptions<DataContext>>()))
+            {
+                context.Database.Migrate();
+                _dataContext = context;
+                await CheckCategories();
+            }
+        }
+
+        private static async Task CheckCategories()
+        {
+            if (_dataContext.Categories.Any())
+            {
+                return;  
+            }
+
+            //await context.Database.EnsureCreatedAsync();
+            await _dataContext.Categories.AddAsync(
+                 new Categories
+                 {
+                     CategoryName = "vegetales",
+                     Description = "vegetales"
+                  });
+
+            await _dataContext.SaveChangesAsync();
+        }
+
+        //public async Task SeedAsync()
+        //{
+        //    await _dataContext.Database.EnsureCreatedAsync();
+        //    //await CheckRoles();
+        //    //var manager = await CheckUserAsync("1010", "Juan", "Zuluaga", "jzuluaga55@gmail.com", "350 634 2747", "Calle Luna Calle Sol", "Admin");
+        //    //var customer = await CheckUserAsync("2020", "Juan", "Zuluaga", "jzuluaga55@hotmail.com", "350 634 2747", "Calle Luna Calle Sol", "Customer");
+        //    //await CheckPetTypesAsync();
+        //    //await CheckServiceTypesAsync();
+        //    //await CheckOwnerAsync(customer);
+        //    //await CheckManagerAsync(manager);
+        //    //await CheckPetsAsync();
+        //    //await CheckAgendasAsync();
+        //}
+    }
+}
--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -2,18 +2,60 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Store.Web.Data;
 
 namespace Store.Web
 {
+    //public class Program
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        CreateHostBuilder(args).Build().Run();
+    //    }
+
+    //    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    //        Host.CreateDefaultBuilder(args)
+    //            .ConfigureWebHostDefaults(webBuilder =>
+    //            {
+    //                webBuilder.UseStartup<Startup>();
+    //            });
+    //}
+
+    //--DOCURMTACTION--
+    //https://docs.microsoft.com/en-us/aspnet/core/security/authorization/secure-data?view=aspnetcore-3.1
+    //https://docs.microsoft.com/es-es/aspnet/core/tutorials/razor-pages/sql?view=aspnetcore-3.1&tabs=visual-studio
+   
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            RunSeeding(host);
+            host.Run();
+        }
+
+        private static void RunSeeding(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    SeedDb.Initialize(services).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
